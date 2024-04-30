0020ddaf953af175133ecbb30fd909a8396b2a3d
AskaSlip <varenaska@gmail.com>
2024-04-30 10:10:39 +0400

    separate all tasks

---
+++ lesson1/hw1_2.js
@@ -0,0 +1,7 @@
+//- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName = 'Hanna';
+let middleName = 'Volodumurivna';
+let lastName = 'Slipchenko';
+let person = lastName + ' ' + firstName + ' ' + middleName;
+console.log(person);
---
+++ lesson1/hw1_4.js
@@ -0,0 +1,7 @@
+
+//- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+let name = prompt('enter your name');
+let fName = prompt('enter your middle name');
+let age = prompt('enter your age')
+console.log(name, fName, age);
